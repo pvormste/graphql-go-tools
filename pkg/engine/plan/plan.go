403aeccab823c0ba4253bb6a1e8d53c97f888f0b
Burak Sezer <burak.sezer.developer@gmail.com>
2022-02-08 15:30:56 +0300

    check node kind before adding a variable

--- pkg/engine/plan/plan.go
+++ pkg/engine/plan/plan.go
@@ -649,8 +649,27 @@ func (v *Visitor) resolveInputTemplates(config objectFetchConfiguration, input *
 			if !v.Operation.OperationDefinitionHasVariableDefinition(v.operationDefinition, variableValue) {
 				break // omit optional argument when variable is not defined
 			}
+			// TODO: Add more test cases to rest_datasource_test for argument handling
+			variableDefinition, exists := v.Operation.VariableDefinitionByNameAndOperation(v.operationDefinition, v.Operation.VariableValueNameBytes(value.Ref))
+			if !exists {
+				break
+			}
+			typeName := v.Operation.ResolveTypeNameBytes(v.Operation.VariableDefinitions[variableDefinition].Type)
+			node, exists := v.Definition.Index.FirstNodeByNameBytes(typeName)
+			if !exists {
+				break
+			}
+			var variablePath []string
+			switch node.Kind {
+			case ast.NodeKindInputObjectTypeDefinition:
+				// FIXME: Do we need to prepend 'variableValue' to path?
+				variablePath = append(variablePath, path...)
+			default:
+				variablePath = append(variablePath, variableValue)
+			}
+
 			variableName, _ = variables.AddVariable(&resolve.ContextVariable{
-				Path: []string{variableValue},
+				Path: variablePath,
 			}, false)
 		case "request":
 			if len(path) != 2 {
